350c081c28564f07436c1a6fe7d5ea7746b2f45d
Giovanna-Cynthia <giovannacynthia34@gmail.com>
2024-11-08 16:40:53 -0300

    update tela de cadastro

--- src/Pages/TelaCadastro.js
+++ src/Pages/TelaCadastro.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Alert, Button, StyleSheet, Text, TextInput, View } from 'react-native'
+import { Alert, Button, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import axios from 'axios';
 
 export default function Cadastro() {
@@ -30,6 +30,16 @@ export default function Cadastro() {
 
   return (
     <View style={styles.container}>
+      <View style={styles.cabecalho}>
+      <Text style={styles.coffee}>Coffee Express</Text>
+      <Image
+      source={require('../assets/img/capuccino.png')} style={styles.image}
+      />
+      </View>
+        
+        <Text style={styles.registro}>Registrar Produto</Text>
+
+        <View style={styles.forms}>
         <Text style={styles.label}>Nome</Text>
         <TextInput 
         style={styles.input} 
@@ -53,8 +63,13 @@ export default function Cadastro() {
         value={preco}
         onChangeText={setPreco}
         />
-
-        <Button title="Cadastrar Produto" onPress={enviarProduto}/>
+        </View>
+        
+        <View style={styles.button}>
+        <TouchableOpacity style={styles.buyButton}>
+            <Text style={styles.buyButtonText} onPress={enviarProduto}>Cadastrar Produto</Text>
+        </TouchableOpacity>
+        </View>
 
     </View>
 
@@ -64,17 +79,60 @@ export default function Cadastro() {
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
+    backgroundColor: "white"
+  },
+  image: {
+    width: 100,
+    height: 100,
+    left: 310
+  },
+  coffee: {
+    fontSize: 30,
+    top: 60,
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "white"
+  },
+  cabecalho: {
+    backgroundColor: "#38241D",
+  },
+  registro: {
+    paddingTop: 30,
+    paddingBottom: 50,
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
   },
   label: {
     fontSize: 18,
-    marginBottom: 5
+    marginBottom: 5,
+    textAlign: "center",
+    fontWeight: "bold",
   },
   input: {
     borderWidth: 1,
     borderColor: "#ccc",
     padding: 10,
     marginBottom: 20,
-    borderRadius: 5
+    borderRadius: 20,
+  },
+  forms: {
+    padding: 10
+  },
+  buyButton: {
+    backgroundColor: '#38241D',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 30,
+  },
+  buyButtonText: {
+    color: '#fff',
+    fontSize: 20,
+    fontWeight: 'bold',
+    textAlign: "center",
+  },
+  button: {
+    paddingLeft: 20,
+    paddingRight: 20
   }
 });
